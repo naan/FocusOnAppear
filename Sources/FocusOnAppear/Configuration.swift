4c44a8661e7517185a74070ba0c544c1a8c910c1
Kazuho Okui <kaz@naan.net>
2024-03-31 14:00:28 -0400

    Add document

--- Sources/FocusOnAppear/Configuration.swift
+++ Sources/FocusOnAppear/Configuration.swift
@@ -1,16 +1,22 @@
 import Foundation
 import SwiftUI
 
+/// A configuration for `focusOnAppear` modifier.
 public struct Configuration {
+    /// Configure the keyboard type for the input field when `focusOnAppear` is applied.
     public let keyboardType: KeyboardType
+    /// Configure the return key type for the input field when `focusOnAppear` is applied.
     public let returnKeyType: ReturnKeyType
+    /// A Boolean value that indicates whether the input field is `SecureField`.
     public let isSecure: Bool
 
+    /// Initialize `Configuration` with the given parameters.
     public init(keyboardType: KeyboardType = .default, returnKeyType: ReturnKeyType = .default, isSecure: Bool = false) {
         self.keyboardType = keyboardType
         self.returnKeyType = returnKeyType
         self.isSecure = isSecure
     }
 
+    /// The default configuration for `focusOnAppear`.
     static public let `default` = Configuration()
 }
--- Sources/FocusOnAppear/FocusOnAppear.swift
+++ Sources/FocusOnAppear/FocusOnAppear.swift
@@ -3,35 +3,39 @@ import SwiftUI
 
 extension View {
 
+    /// Modify this view to set focus when the view appears along with the system keyboard in a single animation.
+    /// - Parameter config: Configure the input traits of the view.
+    /// - Returns: The modified view.
     public func focusOnAppear(config: Configuration = .default) -> some View {
         modifier(FocusOnAppearModifierWithoutCondition(config))
     }
-
-    #if os(iOS)
-
+    
+    /// Modify this view to set focus when the view appears along with the system keyboard in a single animation, and bind its focus state to the provided Boolean state value.
+    /// - Parameters:
+    ///   - condition: The focus state to be bound.
+    ///   - config: Configure the input traits of the view.
+    /// - Returns: The modified view.
     public func focusOnAppear(_ condition: FocusState<Bool>.Binding, config: Configuration = .default) -> some View {
+        #if os(iOS)
         modifier(FocusOnAppearModifier(condition, value: true, config: config))
+        #else
+            focused(condition)
+        #endif
     }
-
+    
+    /// Modifies this view to set focus when the view appears along with the system keyboard in a single animation, and bind its focus state to the given state value.
+    /// - Parameters:
+    ///   - condition: The focus state to be bound.
+    ///   - value: The value to match against when determining whether the binding should change.
+    ///   - config: Configure the input traits of the view.
+    /// - Returns: The modified view.
     public func focusOnAppear<Value>(_ condition: FocusState<Value>.Binding,
                                      equals value: Value,
                                      config: Configuration = .default) -> some View where Value: Hashable {
+        #if os(iOS)
         modifier(FocusOnAppearModifier(condition, value: value, config: config))
+        #else
+            focused(condition, equals: value)
+        #endif
     }
-
-    #else
-
-    public func focusOnAppear(_ condition: FocusState<Bool>.Binding, config: Configuration = .default) -> some View {
-        self
-            .focused(condition)
-    }
-
-    public func focusOnAppear<Value>(_ condition: FocusState<Value>.Binding,
-                                     equals value: Value,
-                                     config: Configuration = .default) -> some View where Value: Hashable {
-        self
-            .focused(condition, equals: value)
-    }
-
-#endif
 }
