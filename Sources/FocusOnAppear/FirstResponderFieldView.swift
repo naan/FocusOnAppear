9afd5b66489c1b7b5b1e339b74b399fa975d12f1
Tomer Beilinson <156321362+tomerbeil@users.noreply.github.com>
2024-06-25 02:40:51 +0300

    Update FirstResponderFieldView.swift
    Added languageCode support

--- Sources/FocusOnAppear/FirstResponderFieldView.swift
+++ Sources/FocusOnAppear/FirstResponderFieldView.swift
@@ -4,18 +4,35 @@ import SwiftUI
 #if os(iOS)
 
 class FirstResponderField: UITextField {
+    var languageCode: String?
 
     init(_ config: Configuration) {
         super.init(frame: .zero)
         keyboardType = config.keyboardType
         returnKeyType = config.returnKeyType
         isSecureTextEntry = config.isSecure
+        languageCode = config.languageCode
         becomeFirstResponder()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    override var textInputMode: UITextInputMode? {
+        if let languageCode = languageCode {
+            for inputMode in UITextInputMode.activeInputModes {
+                if let inputModeLanguage = inputMode.primaryLanguage {
+                    let locale = Locale(identifier: inputModeLanguage)
+                    if locale.languageCode == languageCode {
+                        return inputMode
+                    }
+                }
+            }
+        }
+        return super.textInputMode
+    }
+    
 }
 
 struct FirstResponderFieldView: UIViewRepresentable {
